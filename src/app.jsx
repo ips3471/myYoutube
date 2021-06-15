a7e489bb2d73ae5ac38240bfcd909d667241fbfe
ips3471 <82862685+ips3471@users.noreply.github.com>
2021-06-15 19:51:50 +0900

    make video search header

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './app.css';
 import VideoList from './components/video_list/video_list';
+import VideoSearch from './components/video_search/video_search';
 
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+  };
+  
+  fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyC9LrWrMYN4z95E7FPKSqtfifM5d6u9oY0&key=AIzaSyC9LrWrMYN4z95E7FPKSqtfifM5d6u9oY0`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id: item.id.videoId})) )
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
 
   useEffect(() => {
     const requestOptions = {
@@ -17,9 +30,14 @@ function App() {
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   }, [])
-  // console.log(videos);
 
-  return <VideoList videos={videos} />
+
+  return (
+  <>
+    <VideoSearch onSearch={search}/>
+    <VideoList videos={videos} />
+  </>
+  )
 }
 
 export default App;
---
+++ src/components/video_search/video_search.jsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react';
+import styles from './video_search.module.css';
+
+const VideoSearch = ({onSearch}) => {
+    const inputRef = useRef();
+    const formRef = useRef();
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const value = inputRef.current.value;
+        onSearch(value);
+        formRef.current.reset();
+    }
+
+    return (
+        <form 
+        className={styles.header}
+        onSubmit={onSubmit}
+        ref={formRef}>
+            <span className={styles.logo}>
+                <img 
+                src="./images/logo.png" alt="search-logo"
+                className={styles.logoImage}/>
+                <h1 className={styles.logoName}>Youtube</h1>
+            </span>
+            <input 
+            className={styles.input} 
+            type="search"
+            ref={inputRef}
+            />
+            <button className={styles.searchButton}>
+                <img 
+                src="./images/search.png" 
+                alt="search-icon"
+                className={styles.buttonImage}/>
+            </button>
+        </form>
+    )
+}
+
+
+
+
+export default VideoSearch;
