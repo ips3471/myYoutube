02c9ed0e651cd45c29dabd120d63b92a80367ce1
ips3471 <82862685+ips3471@users.noreply.github.com>
2021-06-16 14:34:45 +0900

    add network service and refac-

--- src/app.jsx
+++ src/app.jsx
@@ -4,31 +4,16 @@ import VideoList from './components/video_list/video_list';
 import VideoSearch from './components/video_search/video_search';
 
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-  };
-  
-  fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyC9LrWrMYN4z95E7FPKSqtfifM5d6u9oY0&key=AIzaSyC9LrWrMYN4z95E7FPKSqtfifM5d6u9oY0`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})) )
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube.search(query)
+    .then(videos => setVideos(videos))
   }
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-  };
-  
-  fetch("https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyC9LrWrMYN4z95E7FPKSqtfifM5d6u9oY0&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC9LrWrMYN4z95E7FPKSqtfifM5d6u9oY0", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube.mostPopular()
+    .then(videos => setVideos(videos))
   }, [])
 
 
